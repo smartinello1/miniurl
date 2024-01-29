c9c7d3cd8e2a341842423d199f6cecd4be2bc4e6
smartinello1 <martinellosimone01@gmail.com>
2024-01-30 00:14:01 +0100

    fix: fixed link and routing on frontend

--- frontend/miniurl/src/App.vue
+++ frontend/miniurl/src/App.vue
@@ -11,8 +11,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/app/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/app2/app">App2</RouterLink>
       </nav>
     </div>
   </header>
